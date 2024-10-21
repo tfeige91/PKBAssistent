448f6a5a1d06a47eafce3dcdde66274803ccbad3
Timfeige <feige_tim@yahoo.de>
2024-10-21 14:14:06 +0200

    Initial Commit

---
+++ AI4PDDemo/AI4PDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI4PDDemoApp.swift
+//  AI4PDDemo
+//
+//  Created by Tim_Feige on 21.10.24.
+//
+
+import SwiftUI
+
+@main
+struct AI4PDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI4PDDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI4PDDemo
+//
+//  Created by Tim_Feige on 21.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
